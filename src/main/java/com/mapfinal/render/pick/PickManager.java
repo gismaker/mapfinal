bbd3c6358f026d70321b0486448c26e161e8344b
孤竹行 <gismail@foxmail.com>
2020-07-05 18:19:01 +0800

    PickManager

---
+++ src/main/java/com/mapfinal/render/pick/PickManager.java
@@ -0,0 +1,26 @@
+package com.mapfinal.render.pick;
+
+import java.util.Map;
+
+public class PickManager {
+
+	private Map<String, String> pickEntities;
+	
+	/**
+	 * 获取实体对象拾取时的渲染颜色
+	 * @param id
+	 * @return
+	 */
+	public String getRenderColor(String id) {
+		return null;
+	}
+	
+	/**
+	 * 获取拾取的实体Id
+	 * @param color
+	 * @return
+	 */
+	public String getPickId(String color) {
+		return null;
+	}
+}
